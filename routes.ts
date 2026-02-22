8cd1db3dbc7aa2c3f197a48e91d204c286223799
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-22 15:30:12 +0530

    added routes.ts file for protected and public route strings

---
+++ routes.ts
@@ -0,0 +1,19 @@
+//public routes
+
+export const publicRoutes: string[] = [
+
+]
+
+//protected routes
+
+export const protectedRoutes: string[]= [
+    "/",
+]
+
+export const authRoles: string[] = [
+    "/auth/sign-in",
+]
+
+export const apiAuthPrefix: string = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT ="/";
